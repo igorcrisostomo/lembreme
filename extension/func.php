7cade9eab9922743523e0547bcc71ac2a7bbfb2a
Riann Batista <riann.batista@gmail.com>
2023-02-10 11:03:34 -0300

    Envio de url para página php

---
+++ extension/func.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    
+  </head>
+  <body>
+  <?php
+
+//essa página está recebendo a url que o usuário passou. Futuramente será enviada para o endpoint 
+
+$variable = $_POST["variable"];
+
+
+echo "Valor da variável: " . $variable;
+?>
+
+    
+  </body>
+</html>
