474ab4da44cb4a18803a0a3c47576634a032c619
Igor Oliveira <igor.oliveira@ufvjm.edu.br>
2023-04-05 13:12:09 -0300

    Criar o .bat para executar a rotina no windows. É necessário inserir o caminho do servidor.py para funcionar corretamente

---
+++ server/desktop.bat
@@ -0,0 +1,4 @@
+# Colocar entre aspas o caminho completo do arquivo servidor.py
+# Exemplo "C:\xampp\htdocs\pun\server\servidor.py"
+# Dessa forma, ao clicar neste arquivo .bat o script em python será executado
+python ""
